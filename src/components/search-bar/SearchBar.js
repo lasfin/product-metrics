533ef3bc2ec63b2b3ea736ef9ccdef3ca1a91b26
Evgenij Kopitsa <e.kopitca@semrush.com>
2016-08-08 12:46:31 +0300

    initial commit

---
+++ src/components/search-bar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>SearchBar will be here</p>
+            </div>
+    );
+    }
+}
+
+export default SearchBar;
