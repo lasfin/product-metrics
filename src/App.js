b1b3fcaf36046a123c51ec09fc357882f886a36e
Evgenij Kopitsa <e.kopitca@semrush.com>
2016-08-08 16:15:53 +0300

    move Header to separate component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from  './components/search-bar/SearchBar';
-import Footer from './components/footer/footer';
-import logo from './logo.svg';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 import './App.css';
 
 class App extends Component {
@@ -9,15 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-wrapper">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Product Metrics Visualization</h2>
-        </div>
-        <SearchBar/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Footer/>
+            <Header/>
+            <SearchBar/>
+            <Footer/>
         </div>
       </div>
     );
