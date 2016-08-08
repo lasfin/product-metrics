9b45084caa18c00918296bf8f4efa5bdce987aaf
Evgenij Kopitsa <e.kopitca@semrush.com>
2016-08-08 16:47:35 +0300

    add material-ui and react-tap-event-plugin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import SearchBar from  './components/search-bar/SearchBar';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-wrapper">
-            <Header/>
-            <SearchBar/>
-            <Footer/>
-        </div>
-      </div>
-    );
-  }
+
+    constructor(){
+        super();
+        injectTapEventPlugin(); // temporary. will go away once the official React version is released
+    }
+
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div className="App">
+                    <div className="App-wrapper">
+                        <Header/>
+                        <SearchBar/>
+                        <Footer/>
+                    </div>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
