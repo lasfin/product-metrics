d2df1960176b2fa1a1e85c7e76bf7f6c83ab3eb3
Evgenij Kopitsa <e.kopitca@semrush.com>
2016-08-08 16:07:52 +0300

    add footer, flexbox layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import SearchBar from  './components/search-bar/SearchBar';
+import Footer from './components/footer/footer';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +8,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="App-wrapper">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Product Metrics Visualization</h2>
         </div>
+        <SearchBar/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Footer/>
+        </div>
       </div>
     );
   }
