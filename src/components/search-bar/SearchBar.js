d51e958ea548362ba82dca486a406d318f525c7c
Evgenij Kopitsa <e.kopitca@semrush.com>
2016-08-08 16:59:53 +0300

    add AutoComplete input for SearchBar

--- src/components/search-bar/SearchBar.js
+++ src/components/search-bar/SearchBar.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import AutoComplete from 'material-ui/AutoComplete';
 
 class SearchBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dataSource: [], // todo
+        };
+    }
+
+    handleUpdateInput = (value) => {
+        this.setState({
+            dataSource: [
+                value
+            ],
+        });
+    };
+
+
     render() {
         return (
             <div>
-                <p>SearchBar will be here</p>
+                <AutoComplete
+                    hintText="Type event name, category or label"
+                    dataSource={this.state.dataSource}
+                    onUpdateInput={this.handleUpdateInput}
+                    maxSearchResults="5"
+                />
             </div>
     );
     }
