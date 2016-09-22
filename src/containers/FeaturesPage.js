5a8789d82a7d1b7e63c265cadccc672eea19b446
Evgenij Kopitsa <e.kopitca@semrush.com>
2016-09-22 15:29:21 +0300

    create FeaturesPage component

---
+++ src/containers/FeaturesPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class FeaturesPage extends Component {
+    render() {
+        return (
+            <div className="featuresPage">
+                Features
+            </div>
+        );
+    }
+}
+
+export default FeaturesPage;
+
+
+
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import EventsPage from './containers/EventsPage';
 import EventPage  from './containers/EventPage';
+import FeaturesPage from './containers/FeaturesPage';
 import App from './components/App';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={EventsPage} />
         <Route path="details/:id" component={EventPage}/>
+        <Route path="features" component={FeaturesPage}/>
     </Route>
 );
