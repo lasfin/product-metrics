2414d2399366f48f9ae7b4adcc24162361abd732
lasfin <evgenij-kopica@yandex.ru>
2016-09-25 11:18:34 +0300

    addEvent page improvements

--- src/components/add-event/AddEvent.js
+++ src/components/add-event/AddEvent.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import DatePicker from 'material-ui/DatePicker';
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import ActionNoteAdd from 'material-ui/svg-icons/action/note-add';
 import './add-event.css';
+
+const buttonStyles = {
+    float: 'right',
+    marginTop: '20px'
+};
 
 const AddEvent = () => {
     return (
         <div className="addEvent">
             <TextField
                 hintText="Feature title"
-                style={{'width': '50%'}}
+                style={{'width': '100%'}}
             />
             <DatePicker
                 hintText="Select date"
                 mode="landscape"
+                autoOk={true}
                 className="selectDate"
+                textFieldStyle={{'width': '100%'}}
+            />
+            <RaisedButton
+                label="Create event"
+                labelPosition="before"
+                primary={true}
+                icon={<ActionNoteAdd/>}
+                style={buttonStyles}
             />
         </div>
     )
